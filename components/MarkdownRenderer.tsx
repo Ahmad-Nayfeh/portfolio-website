001cb7dccfcde500ff40098c1e6c22b73e7726e9
Ahmad Nayfeh <ahmad2224466@gmail.com>
2025-07-13 18:23:22 +0300

    Update MarkdownRenderer.tsx

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -3,16 +3,21 @@
 
 import ReactMarkdown from "react-markdown"
 import rehypeHighlight from "rehype-highlight"
+import rehypeRaw from "rehype-raw" // <--- قم باستيراد المكتبة الجديدة
 
 interface MarkdownRendererProps {
   content: string
 }
 
 export default function MarkdownRenderer({ content }: MarkdownRendererProps) {
-  // Apply the className to a wrapping div
   return (
     <div className="prose dark:prose-invert max-w-none prose-lg">
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+      <ReactMarkdown
+        rehypePlugins={[
+          rehypeRaw, // <--- أضف المكتبة هنا
+          rehypeHighlight
+        ]}
+      >
         {content}
       </ReactMarkdown>
     </div>
